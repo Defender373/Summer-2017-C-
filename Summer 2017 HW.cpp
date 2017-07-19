51e309bdf534abc82d0500015e15fcc1bcca04b0
sainig <gregory9797@gmail.com>
2017-07-18 22:44:59 -0500

    Branch Tree Practice
    First HW. Practicing making Branch Trees.
    
    I had to first create the branch and then insert values into it. I'm not sure if there is a way to create a branch with values already in it.

---
+++ Summer 2017 HW.cpp
@@ -0,0 +1,41 @@
+// 7/18/17
+// used this website http://www.cprogramming.com/tutorial/lesson18.html
+// Binary Tree with a few values added to it.
+struct node
+{
+	int key_value;
+	node *left;
+	node *right;
+};
+
+void insert(int key, node *leaf) {
+	if (key < left->key_value) {
+		if (leaf->left != NULL) {
+			insert(key, leaf->left);
+		}
+		else {
+			leaf->left = new node;
+			leaf->left->key_value = key;
+			leaf->left->left = NULL;
+			leaf->left->right = NULL;
+		}
+	}
+	else if (key >= leaf->key_value) {
+		if (leaf->right != NULL) {
+			insert(key, leaf->left);
+		}
+		else {
+			leaf->right = new node;
+			leaf->right->key_value = key;
+			leaf->right-> left = NULL;
+			leaf->right-> right = NULL;
+		}
+	}
+}
+
+int main() {
+	BstNode* root = Null;
+	insert(root, 15);
+	insert(root, 10);
+	insert(root, 20);
+}
